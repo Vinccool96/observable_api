17226eb0cf586581cf5d2d29bd47fc047913b455
Vincent Girard <Vinccool96@gmail.com>
2022-03-08 14:31:24 -0500

    start

---
+++ lib/observable_api.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "observable_api/version"
+
+module ObservableApi
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_observable_api.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TestObservableApi < Minitest::Test
+  def test_version_number
+    refute_nil ::ObservableApi::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert_equal(true, true || false)
+  end
+end
